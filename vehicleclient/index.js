07300d703b0d6b113af9909d3c4f9946c0bb0fa8
Pranav Kumar <pranav.virgo.1989@gmail.com>
2015-07-06 15:01:54 +0530

    small improvements

--- vehicleclient/index.js
+++ vehicleclient/index.js
@@ -9,7 +9,7 @@ var packets_sent = 0;
 function packetify(){
 	var _packets_slug = content.match(/\*(.|[\n\r])*?#/mg); 
 	if(_packets_slug!=null){
-		console.log(_packets_slug.length+' packets');
+		console.log(_packets_slug.length+' packets found.');
 		packets = _packets_slug;
 	}else{
 		console.log('0 packets');
@@ -28,15 +28,16 @@ client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected...');
 	
 	if(packets.length){
-		console.log("we gonna send "+packets.length+" packets");
+		//console.log("we gonna send "+packets.length+" packets");
 	}
 
-
+	
 
 	setInterval(function(){
 		if(packets_sent<packets.length){
 			client.write(packets[packets_sent]);
 			packets_sent = packets_sent + 1;
+			console.log(packets_sent+" packets sent...\n");		
 		}else{
 			client.destroy();
 		}
